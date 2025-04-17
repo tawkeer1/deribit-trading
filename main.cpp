943b4be51048feb2203b41004ae7a6e8404a3754
tawkeer1 <towqeerahmad111@gmail.com>
2025-04-17 11:21:12 +0000

    second commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include "DeribitClient.hpp"
 #include "DeribitWSClient.hpp"
 #include "WSBroadcastServer.hpp"
-
+#include<bits/stdc++.h>
+using namespace std;
 int main() {
     DeribitClient client("KDdbgieA", "rGZW5VaVsFJ1EzJtjlZRX3pH911W3YMUsW6sI2RXOxk");
 
@@ -9,19 +10,77 @@ int main() {
         cerr << "Auth failed. Exiting.\n";
         return 1;
     }
+    int choice;
+    cout<<"Enter 1 to manage order(place,cancel,getorderbook etc) else enter 2 for getting realtime data from deribit\n";
+    cin>>choice;
+    if(choice == 1){
+        cout<<"Enter the order type you want to place\n";
+        cout<<"1. Place Order\n2. CancelOrder\n3. Modify Order\n4. Get OrderBook\n5. Get Position\n6. Get Open Orders\n";
+        int innerChoice;
+        cin >> innerChoice;
+        switch (innerChoice) {
+        case 1: {
+        string instrument, price, amount;
+        cout << "Enter the instrument name\n";
+        cin >> instrument;
+        cout << "Enter the price\n";
+        cin >> price;
+        cout << "Enter the amount\n";
+        cin >> amount;
+        client.placeOrder(instrument, price, amount);
+        break;
+    }
+    case 2: {
+        string orderId;
+        cout << "Enter the order id to cancel\n";
+        cin >> orderId;
+        client.cancelOrder(orderId);
+        break;
+    }
+    case 3: {
+        string orderIdToModify;
+        int newAmount;
+        double newPrice;
+        cout << "Enter the order id to modify\n";
+        cin >> orderIdToModify;
+        cout << "Enter the new amount\n";
+        cin >> newAmount;
+        cout << "Enter the new price\n";
+        cin >> newPrice;
+        client.modifyOrder(orderIdToModify, newAmount, newPrice);
+        break;
+    }
+    case 4: {
+        string instrumentForOrderBook;
+        cout << "Enter the instrument name for order book\n";
+        cin >> instrumentForOrderBook;
+        client.getOrderBook(instrumentForOrderBook);
+        break;
+    }
+    case 5: {
+        string instrumentForPosition;
+        cout << "Enter the instrument name for position\n";
+        cin >> instrumentForPosition;
+        client.getPosition(instrumentForPosition);
+        break;
+    }
+    case 6: {
+        client.getOpenOrders();
+        break;
+    }
+    default: {
+        cout << "Invalid choice\n";
+        break;
+    }
+}
 
-    client.placeOrder("BTC-PERPETUAL", "50000", "10");
-    // client.cancelOrder("order_id_here");
-    // client.modifyOrder("order_id_here", 30, 25000.0);
-    // client.getOrderBook("BTC-PERPETUAL");
-    // client.getOpenOrders();
-    // client.getPosition("BTC-PERPETUAL");
 
-    // WebSocket
+    }
+    else{
+        cout<<"You have selected to get real time data from deribit. Make sure server is running\n";
+        WSBroadcastServer server;
     
-    WSBroadcastServer server;
-    
-    DeribitWSClient deribitWS([&server](const string& msg) {
+      DeribitWSClient deribitWS([&server](const string& msg) {
         server.broadcast(msg);  // Forward Deribit data to all connected clients
     },"ws://localhost:9002");
     
@@ -31,6 +90,7 @@ int main() {
     cout << "Server running. Visit ws://localhost:9002 from a client.\n";
 
     while (true) this_thread::sleep_for(chrono::seconds(10));  // Keep alive
-
+    }
+    
     return 0;
 }
